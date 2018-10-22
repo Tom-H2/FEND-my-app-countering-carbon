abb87507fc4e216a84ee4ef2bf8fa0da895e38a6
Thomas Harris <lostmt.tom@gmail.com>
2018-10-21 22:40:29 -0700

    feat: Builds FourSquare API call code

--- src/API/index.js
+++ src/API/index.js
@@ -44,6 +44,9 @@ export default class SquareAPI {
   return Helper.simpleFetch("/venues/search", "GET", urlPrams);
 }
   static getVenueDetails(VENUE_ID) {
-    return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET")
+    return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET");
+  }
+  static getVenuePhotos(VENUE_ID) {
+    return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
   }
 }
