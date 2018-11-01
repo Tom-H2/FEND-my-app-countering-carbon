ad773ae23998eca6667b14100896c044e701fbe2
Thomas Harris <lostmt.tom@gmail.com>
2018-11-01 07:23:23 -0700

    feat: Creates codes to replace Hardcoded markers, now placed in Markers.js

--- src/App.js
+++ src/App.js
@@ -14,17 +14,38 @@ class App extends Component {
       zoom: [12]
     };
   }
-  handleMarkerClick = (markers) => {
-    markers.isOpen = true;
-    this.setState({markers: Object.assign(this.set.markers, markers)})
+  //Closes open markers--changes in blue
+  closeOpenMarkers = () => {
+    const markers = this.state.markers.map(marker =>{
+      marker.isOpen = false;
+      marker.clickedOnMarker = false;
+      return marker;
+    });
+    //resets state of markers
+    this.setState({markers:Object.assign(markers, markers)})
   }
+
+//Changes state when marker is clicked --green edits
+openInfoWindowOnClick = (marker) => {
+  const clickedVenue = this.state.venues.find(venue => venue.id===marker.id)
+  //console.log(clickedVenue)
+  SquareAPI.getVenueDetails(marker.id).then(results => {
+    const mergedVenueData = Object.assign(clickedVenue, results.response.venue);
+    this.setState({venues: Object.assign(this.state.venues, mergedVenueData)})
+  })
+  .catch(error => {
+    this.setState({error})
+    console.log(this.state.error)
+  })
+}
+
 
     componentDidMount() {
       SquareAPI.search({
         near:"Sequim, WA", //Sequim is a placeholder but will be near marker
         query: "Coffee",
         radius: 800,
-        limit: 40
+        limit: 10
       }).then(results => {
         console.log(results);
       });
@@ -36,6 +57,7 @@ class App extends Component {
             <h1>Countering Carbon</h1>
             <h2>What to do while charging your EV</h2>
           <Map
+          {...this.state}
           handleMarkerClick={this.handleMarkerClick}/>
       </div>
     );
--- src/components/Map.js
+++ src/components/Map.js
@@ -13,58 +13,39 @@ import {
       defaultZoom={10}
       defaultCenter={{lat: 48.100, lng: -123.000}}
     >
-      {props.isMarkerShown &&
-        <Marker
-          title={'Clallam Coop'}
-          position={{lat: 48.080, lng: -123.100}}
-          onClick={console.log}>
-            {Marker.isOpen && <InfoWindow>
-              <p>Clallam Coop</p>
-            </InfoWindow>}
-        </Marker>}
-      {props.isMarkerShown &&
-        <Marker
-          title={'Nicola Engineering'}
-          position={{lat: 48.080, lng: -123.106}}
-          onClick={console.log}>
-            {Marker.isOpen && <InfoWindow>
-              <p>Nicola Engineering</p>
-            </InfoWindow>}
-        </Marker>}
-      {props.isMarkerShown &&
-        <Marker
-          title={'Port Angeles Waterfront'}
-          position={{lat: 48.1215, lng: -123.434}}
-          onClick={console.log}>
-            {Marker.isOpen && <InfoWindow>
-              <p>Nicola Engineering</p>
-            </InfoWindow>}
-        </Marker>}}
-      {props.isMarkerShown &&
-        <Marker
-          title={'Olympic Medical Center'}
-          position={{lat: 48.115, lng: -123.415}}
-          onClick={console.log}>
-            {Marker.isOpen && <InfoWindow>
-              <p>Olympic Medical Center</p>
-            </InfoWindow>}
-        </Marker>}
-      {props.isMarkerShown &&
-        <Marker
-          title={'Disco Bay Junction'}
-          position={{lat: 47.990, lng: -122.891}}
-          onClick={console.log}>
-            {Marker.isOpen && <InfoWindow>
-              <p>Disco Bay Junction</p>
-            </InfoWindow>}
-        </Marker>}
+      {props.markers && props.markers.filter(marker => marker.isVisible).map((marker,id) => {
+        const venueInfo = props.venues.find(venue =>(venue.id ===marker.id))
+        return(
+          <Marker
+            key={id}
+            id="markerPin"
+            position={{lat: marker.lat, lang: marker.lng}}
+            onClick={() => props.handleMarkerClick(marker)}>
+            {marker.isOpen && (
+              <InfoWindow>
+                <React.Fragment>
+                  <div className="info-window">
+                    <p className="info-title">{venueInfo.name}</p>
+                    <p className="info-subtext">{venueInfo.location.formattedAddress[0]}</p>
+                    <p className="info-subtext">{venueInfo.location.formattedAddress[1]}</p>
+                  </div>
+                </React.Fragment>
+              </InfoWindow>
+              )}
+            </Marker>
+        )
+      })}
     </GoogleMap>
   ))
 
     class Map extends Component {
+      componentWillReceiveProps = (props) => {
+        this.props = props
+      }
       render() {
         return (
           <MyMapComponent
+            {...this.props}
             isMarkerShown
             googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDeiczf4IikLo9sb8zfCtNQPHzHcakh-dg"
             loadingElement={<div style={{ height: `100%` }} />}
