d58bcb1322f2f0a2ba76523d10ed61168469e7e4
Thomas Harris <lostmt.tom@gmail.com>
2018-11-03 08:55:21 -0700

    style: Adds styling to side bar so it opens and closes correctly.

--- src/components/Map.js
+++ src/components/Map.js
@@ -46,14 +46,16 @@ import {
       }
       render() {
         return (
-          <MyMapComponent
-            {...this.props}
-            isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDeiczf4IikLo9sb8zfCtNQPHzHcakh-dg"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-          />
+          <div className="map">
+            <MyMapComponent
+              {...this.props}
+              isMarkerShown
+              googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyDeiczf4IikLo9sb8zfCtNQPHzHcakh-dg"
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `400px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+            />
+          </div>
         );
       }
     }
