d3aa155766e90b885fb21e621717fb42124a3fa0
Thomas Harris <lostmt.tom@gmail.com>
2018-11-12 10:57:18 -0800

    refactor: Makes code into object in order to run more smoothly

--- src/API/index.js
+++ src/API/index.js
@@ -1,9 +1,10 @@
 //Based on Forrest video https://www.youtube.com/watch?v=Dj5hzKBxCBI&index=3&list=PL4rQq4MQP1crXuPtruu_eijgOUUXhcUCP
-class Helper {
-  static baseURL() {
-    return "https://api.foursquare.com/v2"
-  }
-  static auth() {
+//update with the advice of Jason Michael White
+const Helper = {
+  baseURL: () => {
+    return "https://api.foursquare.com/v2";
+  },
+  auth: () => {
     const keys = {
       client_id:"JPFAJLT3GCHEWOCLXYNVDL0XXNXQ2525K2KXEBO5IEYB4UOD",
       client_secret:"NBHHGTR40PTJXCPKQ3BAM4R42YWPIWOUMI13K1CJO1S5EYG1",
@@ -11,22 +12,22 @@ class Helper {
     };
     return Object.keys(keys)
       .map(key=> `${key}=${keys[key]}`)
-      .join('&')
-  }
-  static urlBuilder(urlPrams) {
+      .join('&');
+  },
+  urlBuilder: (urlPrams) => {
     if(!urlPrams){
-      return ""
+      return "";
     }
     return Object.keys(urlPrams)
       .map(key => `${key}=${urlPrams[key]}`)
       .join('&');
-  }
-  static headers() {
+  },
+  headers: () => {
     return {
       Accept: "application/json"
     };
-  }
-  static simpleFetch(endPoint, method, urlPrams) {
+  },
+  simpleFetch: (endPoint, method, urlPrams) => {
     let requestData = {
       method,
       headers: Helper.headers()
@@ -35,19 +36,21 @@ class Helper {
       `${Helper.baseURL()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(urlPrams)}`,
       requestData
     ).then(res => res.json())
-     
+
 .catch(error => console.error('Error:', error));
   }
-}
+};
 
-export default class SquareAPI {
-  static search(urlPrams) {
+const SquareAPI = {
+  search: urlPrams => {
   return Helper.simpleFetch("/venues/search", "GET", urlPrams);
-}
-  static getVenueDetails(VENUE_ID) {
+},
+  getVenueDetails: VENUE_ID => {
     return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET");
-  }
-  static getVenuePhotos(VENUE_ID) {
+  },
+  getVenuePhotos: VENUE_ID => {
     return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
   }
-}
+};
+
+export default SquareAPI;
